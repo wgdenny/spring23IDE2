243ff41cf71993665a4aa08a3f5557d4bd6a08e2
Will Denny <sparky.wgd@gmail.com>
2023-03-21 17:42:01 -0400

    Finished the gas class and tested objects

--- src/Gas.java
+++ src/Gas.java
@@ -5,6 +5,8 @@ public class Gas {
     private String carName;
     private double financePayment;
     private double MPG;
+    private Double totalMonthlyCost;
+
 
     //constructor
     public Gas(String name, Double payment, Double mileage){
@@ -24,6 +26,40 @@ public class Gas {
         return carName;
     }//ends our get name
 
-    //public void (Double setFinancePayment){
+    public void setFinancePayment(Double payment){
+        financePayment = payment;
+    }
+    //getter for finance
+    public Double getFinancePayment(){
+        return financePayment;
+    }//ends get finance
+
+    public void setMPG(Double mileage){
+        MPG = mileage;
+    }
+    //getter for Milage
+    public Double getMPG(){
+        return MPG;
+    }
+    //need to do calculations for our monthly cost
+    //setTotalMonthlyCost
+    public void setTotalMonthlyCost(Double milesMonthly, Double gasCost){
+        Double totalCost = this.financePayment + (milesMonthly/this.MPG * gasCost);
+        totalMonthlyCost = totalCost;
+
+    }//ends our setTotalMonthlyCost
+    //getter
+    public Double getTotalMonthlyCost(){
+        return totalMonthlyCost;
+    }//ends get
+    //overide java toString() method
+    @Override
+    public String toString(){
+        return "\n" + carName + " Total Monthly Costs: " + totalMonthlyCost;
 
     }
+
+
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,31 @@ Gas/Electric vehicle comparison using OOP principles
 
 public class Main {
     public static void main(String[] args) {
-        Gas car1 = new Gas("Test Car 1", 251.23, 23.5);
-        Gas car2 = new Gas("Test Car 2", 293.23, 30.5);
+        Gas car1 = new Gas("Test Car 1", 467.45, 34.5);
+        Gas car2 = new Gas("Test Car 2", 583.14, 50.0);
+
+        car1.setTotalMonthlyCost(100.00, 3.12);
+        car1.setTotalMonthlyCost(100.00, 3.12);
+
+
+        System.out.println(car1.toString());
+        System.out.println(car2.toString());
+        
+        //System.out.println(car1.toString());
 
         System.out.println("Here is the car name for car1: " + car1.getName());
-        System.out.println("Here is the car name for car2: " + car2.getName());
+        //System.out.println("Here is the car name for car2: " + car2.getName());
+        //more testing for our gas class objects
+
+        car1.setTotalMonthlyCost(890.00, 3.14);
+        System.out.println(car1.getTotalMonthlyCost());
+        //car1.getTotalMonthlyCost(555.51);
+        System.out.println(car1.getFinancePayment());
+
+
+
+
+
+
     }
 }
